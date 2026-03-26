5e93e4a878e113746ad6c9edda381c494fabed29
arushxsingh <arushkumar054@gmail.com>
2026-03-26 22:52:09 +0530

    fix

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ if "GROQ_API_KEY" in __import__("streamlit").secrets:
 
 from langchain_community.document_loaders import PDFPlumberLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_community.embeddings import HuggingFaceEmbeddings
+from langchain_huggingface import HuggingFaceEmbeddings
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.vectorstores import FAISS
 from langchain_groq import ChatGroq
